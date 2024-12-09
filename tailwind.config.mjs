a6575e2318d5b5135ee203222024a5df38ca57a4
Henry <168932407+ELperrocode@users.noreply.github.com>
2024-12-09 15:13:58 -0500

    feat: add projects component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode:'class',
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  colors: {
+			background: '#020411',//richblack
+			secondary: '#011936',//charcoal
+			accent: '#9CD08F',//pistachio
+			primary: '#96C5F7',//columbia blue
+			foreground: '#103900',//pakistan green
+			//#272635 rising black'
+
+		  },
+		},
+	  },
 	plugins: [],
 }
 
